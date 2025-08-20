97b54673778779ce7caa5ec1292c989a35d9a38f
Aki <akihirai@gmail.com>
2025-08-20 23:18:38 +0300

    203 Добавлен контроллер с get-методом получения всех продуктов

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using store_api.Data;
+using store_api.Models;
+
+namespace store_api.Controllers;
+
+public class ProductController(AppDbContext dbContext) : StoreController(dbContext)
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetAllProducts()
+    {
+        var products = await dbContext.Products.AsNoTracking().ToListAsync();
+        return Ok(products);
+    }
+}
--- Api/Controllers/StoreController.cs
+++ Api/Controllers/StoreController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using store_api.Data;
 
 namespace store_api.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class StoreController : ControllerBase
+    public class StoreController(AppDbContext dbContext) : ControllerBase
     {
+        protected AppDbContext dbContext = dbContext;
     }
 }
