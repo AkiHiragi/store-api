e2db77083fa7dce59f606a1df366eaccbb9e5672
Aki <akihiragi@gmail.com>
2025-08-22 15:00:44 +0300

    205 Добавлен эндпоинт получения продукта по id

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -11,10 +11,35 @@ namespace store_api.Controllers;
 public class ProductController(AppDbContext dbContext) : StoreController(dbContext)
 {
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetAllProducts()
+    public async Task<IActionResult> GetAllProducts()
     {
         var products = await dbContext.Products.AsNoTracking().ToListAsync();
         var response = new ResponseServer { StatusCode = HttpStatusCode.OK, Result = products };
         return Ok(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProductById(int id)
+    {
+        if (id <= 0)
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = ["Неверный id"]
+            });
+
+        var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.NotFound,
+                ErrorMessages = ["Продукт по указанному id не найден"]
+            });
+        }
+
+        return Ok(new ResponseServer { StatusCode = HttpStatusCode.OK, Result = product });
     }
 }
