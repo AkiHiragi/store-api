811d66ac17dd2482a4b3f8e14c7b18a7365a8a75
Aki <akihiragi@gmail.com>
2025-08-22 16:34:44 +0300

    207 Добавлен эндпоинт добавления продукта

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +19,7 @@ public class ProductController(AppDbContext dbContext) : StoreController(dbConte
         return Ok(response);
     }
 
-    [HttpGet]
+    [HttpGet("{id}", Name = nameof(GetProductById))]
     public async Task<IActionResult> GetProductById(int id)
     {
         if (id <= 0)
@@ -44,9 +44,62 @@ public class ProductController(AppDbContext dbContext) : StoreController(dbConte
         return Ok(new ResponseServer { StatusCode = HttpStatusCode.OK, Result = product });
     }
 
-    // [HttpPost]
-    // public async Task<ActionResult<ResponseServer>> CreateProduct([FromBody] ProductCreateDto dto)
-    // {
-    //     
-    // }
+    [HttpPost]
+    public async Task<ActionResult<ResponseServer>> CreateProduct([FromBody] ProductCreateDto dto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (dto.Image == null || dto.Image.Length == 0)
+                {
+                    return BadRequest(new ResponseServer
+                    {
+                        IsSuccess = false,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorMessages = ["Изображение не может быть пустым"]
+                    });
+                }
+
+
+                var item = new Product
+                {
+                    Name = dto.Name,
+                    Description = dto.Description,
+                    SpecialTag = dto.SpecialTag,
+                    Category = dto.Category,
+                    Price = dto.Price,
+                    Image = $"https://place-hold.it/{Random.Shared.Next(200, 500)}x{Random.Shared.Next(200, 500)}"
+                };
+
+                await dbContext.Products.AddAsync(item);
+                await dbContext.SaveChangesAsync();
+
+                var response = new ResponseServer
+                {
+                    IsSuccess = true,
+                    StatusCode = HttpStatusCode.Created,
+                    Result = item
+                };
+
+                return CreatedAtRoute(nameof(GetProductById), new { id = item.Id }, response);
+            }
+
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = ["Модель данных не подходит"]
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = ["Что-то поломалось", ex.Message]
+            });
+        }
+    }
 }
